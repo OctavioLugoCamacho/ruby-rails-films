6c5f8790bf9d38e92c2fc656946052aa76b8c2bf
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-07 09:33:51 -0500

    Add the delete function in the controller and in the view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,6 +27,12 @@ class MoviesController < ApplicationController
     redirect_to @movie
   end
 
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_url
+  end
+
   private
     def movie_params
       params.require(:movie).
