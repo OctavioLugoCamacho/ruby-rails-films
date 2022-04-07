a0b5bcb78adea976a8cc289a6b66c00373f58bc6
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-07 11:46:21 -0500

    Add the new params to the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_040218) do
+ActiveRecord::Schema.define(version: 2022_04_07_161845) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2022_04_06_040218) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
     t.date "released_on"
+    t.string "director"
+    t.string "duration"
+    t.string "image_file_name"
   end
 
 end
