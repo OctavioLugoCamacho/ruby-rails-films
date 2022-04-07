1f7bc292c0d27c0bbf6f93e849f4745410f579f2
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-07 11:46:40 -0500

    Add the functions to update and save the new params in events

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -36,6 +36,7 @@ class MoviesController < ApplicationController
   private
     def movie_params
       params.require(:movie).
-        permit(:title, :description, :rating, :released_on, :total_gross)
+        permit(:title, :description, :rating, :released_on, :total_gross,
+          :director, :duration, :image_file_name)
     end
 end
