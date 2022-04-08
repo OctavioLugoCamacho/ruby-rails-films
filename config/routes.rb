f9e272a6bf2d518c6ceaee42bd1f1ee7acf8b1cc
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-07 21:43:38 -0500

    Create the reviews database that belongs to movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :reviews
   root "movies#index"
   resources :movies
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_07_161845) do
+ActiveRecord::Schema.define(version: 2022_04_08_023929) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -25,4 +25,15 @@ ActiveRecord::Schema.define(version: 2022_04_07_161845) do
     t.string "image_file_name"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.integer "stars"
+    t.text "comment"
+    t.integer "movie_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
+  end
+
+  add_foreign_key "reviews", "movies"
 end
