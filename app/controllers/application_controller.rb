a6fcb218c6190ebfc4fa2dbd00c9dc7833486532
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-11 18:31:29 -0500

    Add the restrictions for the current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def require_signin
+    unless current_user
+      redirect_to new_session_url, alert: "Please sign in first!"
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
 end
