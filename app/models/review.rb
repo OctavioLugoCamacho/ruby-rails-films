ac9409891e6c9050bd367e3fce522da810ecbee0
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-12 16:36:39 -0500

    Add the relationship between user, movie and reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :movie
-  validates :name, presence: true
+  belongs_to :user
   validates :comment, length: { minimum: 4 }
   STARS = [1, 2, 3, 4, 5]
   validates :stars, inclusion: {
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :reviews, dependent: :destroy
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true,
