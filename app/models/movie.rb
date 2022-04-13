c19103368c533feb2ee2004eb648a6ff9a9cbe3d
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-13 17:50:57 -0500

    Add the genre and characterizations resources

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,9 @@ class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :fans, through: :favorites, source: :user
+  has_many :characterizations, dependent: :destroy
+  has_many :genres, through: :characterizations
+
   RATINGS = %w(G PG PG-13 R NC-17)
   validates :title, :released_on, :duration, presence: true
   validates :description, length: { minimum: 25 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :genres
   root "movies#index"
   resources :movies do
     resources :reviews
