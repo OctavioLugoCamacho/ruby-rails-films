2dd12c3a4ba915f779c5e8adb3ce12bc71ea09cf
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-13 18:05:26 -0500

    Add the checkboxes in the form to update/cremovies with genres

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,7 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     @fans = @movie.fans
+    @genres = @movie.genres.order(:name)
     if current_user
       @favorite = current_user.favorites.find_by(movie_id: @movie.id)
     end
@@ -50,6 +51,6 @@ class MoviesController < ApplicationController
     def movie_params
       params.require(:movie).
         permit(:title, :description, :rating, :released_on, :total_gross,
-          :director, :duration, :image_file_name)
+          :director, :duration, :image_file_name, genre_ids: [])
     end
 end
