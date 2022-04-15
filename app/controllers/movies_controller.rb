4d5880c70a0038f84d294b6bc0028999020c3ac0
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-04-14 20:23:03 -0500

    Add the filtered movies links with custome scopes and routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,18 @@ class MoviesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
 
   def index
-    @movies = Movie.released
+    case params[:filter]
+    when "upcoming"
+      @movies = Movie.upcoming
+    when "recent"
+      @movies = Movie.recent
+    when "hits"
+      @movies = Movie.hits
+    when "flops"
+      @movies = Movie.flops
+    else
+      @movies = Movie.released
+    end
   end
 
   def show
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -12,4 +12,12 @@ module MoviesHelper
     movie.released_on.year
   end
 
+  def nav_link_to(text, url)
+    if current_page?(url)
+      link_to(text, url, class: "active")
+    else
+      link_to(text, url)
+    end
+  end
+
 end
